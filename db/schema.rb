87b688e989dd6fe4f221833adad887fbbf625b57
Pauline Gaget <lupiane@hotmail.com>
2017-04-18 11:53:26 +0200

    set completed default as false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418091308) do
+ActiveRecord::Schema.define(version: 20170418094949) do
 
   create_table "task_managers", force: :cascade do |t|
     t.string   "name"
@@ -23,9 +23,9 @@ ActiveRecord::Schema.define(version: 20170418091308) do
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.string   "deadline"
-    t.boolean  "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "completed",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
