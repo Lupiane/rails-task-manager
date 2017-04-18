76500b9b1c503d24f2540c92ab8479b844a160d2
Pauline Gaget <lupiane@hotmail.com>
2017-04-18 14:04:45 +0200

    changed deadline type to date

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418094949) do
+ActiveRecord::Schema.define(version: 20170418115624) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
-    t.string   "deadline"
+    t.date     "deadline"
     t.boolean  "completed",  default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
