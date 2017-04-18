011f688d7d2640c3266cdadab442a9bc123e21db
Pauline Gaget <lupiane@hotmail.com>
2017-04-18 14:22:43 +0200

    tasks ordered by deadline with first one displayed in red

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Task.create(name: "Call Insurance company", deadline: "18 April")
-Task.create(name: "Book appointment at hairdresser's", deadline: "12 May")
-Task.create(name: "Clean flat", deadline: "22 April")
+Task.create(name: "Call Insurance company", deadline: "18/04/2017")
+Task.create(name: "Book appointment at hairdresser's", deadline: "12/05/2017")
+Task.create(name: "Clean flat", deadline: "22/04/2017")
